5fe6f5136ad6a94be607ee045773a07fd9149d41
qipeng <qp19940413@163.com>
2018-05-10 16:33:42 +0800

    add config center

--- config_server/src/main/java/com/qipeng/spring_cloud_demo/ConfigServerApplication.java
+++ config_server/src/main/java/com/qipeng/spring_cloud_demo/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.qipeng.spring_cloud_demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer // 开启配置服务功能
+@EnableEurekaClient
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
